f8faae203ef0aa974c46d134f470bec109e702de
Dikaarfn <andhikaarfn@gmail.com>
2021-09-24 15:25:20 +0700

    Tugas Pertemuan 1

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama Saya Andhika Rifqi A
+        Saya tinggal di Bogor, Jawa Barat
+        olahraga yg saya sukai adalah
+        Berenang";
+    }
+}
