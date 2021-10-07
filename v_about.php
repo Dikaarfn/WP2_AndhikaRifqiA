d59dcf5f1773a37535a81e76275a2e550529d113
Dikaarfn <77535652+Dikaarfn@users.noreply.github.com>
2021-10-07 19:46:21 +0700

    Upload Pertemuan 3
    Website menggunakan Template Sederhana dangan Codeigniter

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <ul type="disc">
+        <li>Nama Depan : Andhika</li>
+        <li>Nama Belakang : Arifin</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Jupiter VII</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepak bola</li>
+        <li>Renang</li>
+    </ul>
+</section>
